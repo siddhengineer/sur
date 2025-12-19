82cbc5f6c43b57dd130a9f00bada0e9297ea0b58
kalp3092 <kalp3092@gmil.com>
2025-12-19 08:50:07 +0530

    v2

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
 from fastapi import FastAPI
-from api.route.detection import router as detection_router
 
 app = FastAPI(title="Shoplifting Detection API")
-
-app.include_router(detection_router)
 
 @app.get("/")
 def root():
